85a417b82c2aa0eae3cde26a382e0663f363452d
raamweinfeld <raamweinfeld@gmail.com>
2016-01-01 23:45:59 -0700

    IT BEGINS

---
+++ Global Mech Patriotism!/Assets/LegController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegController : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float legMovement = Input.GetAxis("Vertical");
+
+        rb.AddForce(Vector3.forward * legMovement * 100);
+	}
+}
